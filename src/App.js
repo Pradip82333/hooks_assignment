21f28dad2754a0c6a80484dfa5f9149a26873877
SAITEJA-Boyapally-AD <Bhatin626@gmail.com>
2023-03-14 11:01:42 +0530

    Task=> TODO list APP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 
+// task=> Create a to do list with full 
+// functionality using react js and hooks.
+// please do not use any getelementby ID or any vanilla js functions.
+
 
 function App() {
   return <div className="App">
+
 </div>;
 }
 
